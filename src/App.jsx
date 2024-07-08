632e914ae9a70c86bfeda6f7fb04d2e1b111f659
박지훈 <pjh@ziumks.com>
2024-07-08 15:22:54 +0900

    useDebounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from "react";
-import useInterval from "./hooks/useInterval/useInterval";
+import { Fragment, useState } from "react";
+import useDebounce from "./hooks/useDebounce/useDebounce";
+
+const companies = ["kakao", "Naver", "Daangn", "Coupang", "Line"];
 
 function App() {
-  const [array, setArray] = useState([]);
-  const clear = useInterval(() => {
-    setArray([...array, "추가됨!"]);
-  }, 1000);
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState([]);
+
+  useDebounce(
+    () => {
+      if (value === "") setResult([]);
+      else {
+        setResult(
+          companies.filter((company) =>
+            company.toLowerCase().includes(value.toLowerCase())
+          )
+        );
+      }
+    },
+    300,
+    [value]
+  );
 
   return (
-    <>
-      <div>useInterval 테스트</div>
-      <div>{array}</div>
-      <button onClick={clear}>정지</button>
-    </>
+    <div>
+      <h1>대기업 검색 해보슈</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <div>
+        {result.map((item) => (
+          <Fragment key={item}>
+            {item}
+            <br />
+          </Fragment>
+        ))}
+      </div>
+    </div>
   );
 }
 
