60c2c3f52e6c6689f4e4349733a035f484f15f59
박지훈 <pjh@ziumks.com>
2024-07-08 11:11:25 +0900

    useKey

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
-import useScroll from "./hooks/useScroll/useScroll";
+import useKey from "./hooks/useKey/useKey";
 
 function App() {
-  const [ref, coord] = useScroll();
+  useKey("keydown", "f", () => {
+    alert("f key down");
+  });
 
-  return (
-    <>
-      <div
-        style={{
-          width: "100px",
-          height: "100px",
-          backgroundColor: "red",
-          overflow: "auto",
-        }}
-        ref={ref}
-      >
-        <div
-          style={{
-            width: "10000px",
-            height: "10000px",
-            backgroundImage: "linear-gradient(180deg, #000 0%, #fff 100%)",
-          }}
-        ></div>
-      </div>
-      <button
-        onClick={() => {
-          ref.current.scrollTo({ top: 20000, left: 20000, behavior: "smooth" });
-        }}
-      >
-        scroll
-      </button>
-      {coord.x}, {coord.y}
-    </>
-  );
+  useKey("keyup", "q", () => {
+    alert("q key up");
+  });
+
+  return <>f와 q를 눌러보세요</>;
 }
 
 export default App;
