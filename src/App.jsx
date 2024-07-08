199f0d074c35539462e6589b85e5d1fdec141c00
박지훈 <pjh@ziumks.com>
2024-07-08 15:37:50 +0900

    useAsyncFn(수동시작)

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,57 @@
-import { Fragment, useState } from "react";
-import useDebounce from "./hooks/useDebounce/useDebounce";
+import useAsyncFn from "./hooks/useAsync/useAsyncFn";
 
-const companies = ["kakao", "Naver", "Daangn", "Coupang", "Line"];
+const asyncReturnValue = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Success");
+    }, 1000);
+  });
+};
 
-function App() {
-  const [value, setValue] = useState("");
-  const [result, setResult] = useState([]);
+const asyncReturnError = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject("Error");
+    }, 1000);
+  });
+};
 
-  useDebounce(
-    () => {
-      if (value === "") setResult([]);
-      else {
-        setResult(
-          companies.filter((company) =>
-            company.toLowerCase().includes(value.toLowerCase())
-          )
-        );
-      }
-    },
-    300,
-    [value]
-  );
+const Success = () => {
+  const [state, callback] = useAsyncFn(async () => {
+    return await asyncReturnValue();
+  }, []);
 
   return (
     <div>
-      <h1>대기업 검색 해보슈</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <div>
-        {result.map((item) => (
-          <Fragment key={item}>
-            {item}
-            <br />
-          </Fragment>
-        ))}
-      </div>
+      <div>useAsyncFn 테스트</div>
+      <div>{JSON.stringify(state)}</div>
+      <button onClick={callback} disabled={state.isLoading}>
+        비동기 호출
+      </button>
+    </div>
+  );
+};
+
+const Error = () => {
+  const [state, callback] = useAsyncFn(async () => {
+    return await asyncReturnError();
+  }, []);
+
+  return (
+    <div>
+      <div>useAsyncFn 테스트</div>
+      <div>{JSON.stringify(state)}</div>
+      <button onClick={callback} disabled={state.isLoading}>
+        비동기 호출
+      </button>
+    </div>
+  );
+};
+function App() {
+  return (
+    <div>
+      <Success />
+      <Error />
     </div>
   );
 }
