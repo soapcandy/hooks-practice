9e3e95a1275b0e4c160d4a435167979980a58486
박지훈 <pjh@ziumks.com>
2024-07-08 14:23:00 +0900

    useForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
-import useSessionStorage from "./hooks/useSessionStorage/useSessionStorage";
+import useForm from "./hooks/useForm/useForm";
+
+const sleep = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), 1000);
+  });
+};
 
 function App() {
-  const [status, setStatus] = useSessionStorage("status", "404 NOT FOUND");
+  const { isLoading, errors, handleChange, handleSubmit } = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async (values) => {
+      await sleep();
+      alert(JSON.stringify(values));
+    },
+    validate: ({ email, password }) => {
+      const errors = {};
+      if (!email) errors.email = "이메일을 입력해주세요.";
+      if (!password) errors.password = "비밀번호를 입력해주세요.";
+      if (!/^.+@.+\..+$/.test(email))
+        errors.email = "올바른 이메일을 입력해주세요.";
+      return errors;
+    },
+  });
 
   return (
-    <div>
-      <button onClick={() => setStatus("200 OK")}>Resend</button>
-      {status}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>Sign In</h1>
+      <div>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+        {errors.email}
+      </div>
+      <div>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        {errors.password}
+      </div>
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? "Loading..." : "Submit"}
+      </button>
+    </form>
   );
 }
 
