0d2a549b8ff6707ee26fd3497acab57cd7976e6d
박지훈 <pjh@ziumks.com>
2024-07-08 14:30:57 +0900

    useTimeOutFn(수동작동)

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,15 @@
-import useForm from "./hooks/useForm/useForm";
-
-const sleep = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(), 1000);
-  });
-};
+import useTimeOutFn from "./hooks/useTimeOut/useTimeOutFn";
 
 function App() {
-  const { isLoading, errors, handleChange, handleSubmit } = useForm({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: async (values) => {
-      await sleep();
-      alert(JSON.stringify(values));
-    },
-    validate: ({ email, password }) => {
-      const errors = {};
-      if (!email) errors.email = "이메일을 입력해주세요.";
-      if (!password) errors.password = "비밀번호를 입력해주세요.";
-      if (!/^.+@.+\..+$/.test(email))
-        errors.email = "올바른 이메일을 입력해주세요.";
-      return errors;
-    },
-  });
-
+  const [run, clear] = useTimeOutFn(() => {
+    alert("launch");
+  }, 3000);
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Sign In</h1>
-      <div>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        {errors.email}
-      </div>
-      <div>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          onChange={handleChange}
-        />
-        {errors.password}
-      </div>
-      <button type="submit" disabled={isLoading}>
-        {isLoading ? "Loading..." : "Submit"}
-      </button>
-    </form>
+    <>
+      <div>useTimeOutFn 테스트</div>
+      <button onClick={run}>3초 뒤 실행</button>
+      <button onClick={clear}>정지</button>
+    </>
   );
 }
 
